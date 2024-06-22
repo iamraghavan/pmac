48322e430503b652e074c94321c3cc56a45db7e1
Raghavan <87792550+iamraghavan@users.noreply.github.com>
2024-06-22 09:26:14 +0530

    Update settings.py

--- pmac/settings.py
+++ pmac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wwmplt-*f8f6w4q@(*8#fo9(b(uod9d5jov=da!0c=)c9p1l^^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # AUTH_USER_MODEL = 'pmadmin.AdminUsers'
 
