8643e7d4935d8585f7443d594c8146aa604a60f3
iam.raghavan <developer.raghavan@gmail.com>
2024-06-23 21:47:39 +0530

    Refactor debug mode and URLs for admin control in pmadmin app

--- pmac/urls.py
+++ pmac/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('o/secure/<str:unique_id>/admin-control/profile/',include('pmadmin.urls')),
     path('o/secure/<str:unique_id>/admin-control/payment-status/',include('pmadmin.urls')),
     path('o/secure/<str:unique_id>/admin-control/profile-view/',include('pmadmin.urls')),
-    path('o/secure/<str:unique_id>/admin-control/profile-view/', include('pmadmin.urls')),
-    path('edit-profile/<str:user_pmid>/', include('pmadmin.urls')),
+
+    
 ]
